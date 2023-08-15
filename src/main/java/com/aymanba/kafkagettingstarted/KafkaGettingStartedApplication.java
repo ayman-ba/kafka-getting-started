2cb7351da820489c69baba43286080144ec290c9
Ayman BEN AMOR <ayman92.benamor@gmail.com>
2023-08-15 13:09:26 +0200

    initial commit

---
+++ src/main/java/com/aymanba/kafkagettingstarted/KafkaGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.aymanba.kafkagettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGettingStartedApplication.class, args);
+    }
+
+}
